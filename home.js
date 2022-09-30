d8aa850a8ae5c60326f1b3e8119d060693e4d709
Moumita Das <dasmoumita.me@gmail.com>
2022-09-30 22:37:15 +0530

    day-4

--- home.js
+++ home.js
@@ -27,3 +27,74 @@ left_btn.addEventListener("click",()=>{
     slimg.src=imglink[imgState]
     console.log(imgState)
 })
+
+
+
+{/* <script type="text/javascript"> */}
+//   $(document).ready(function() {
+//     $("#lightSlider").lightSlider(); 
+//   });
+// </script>
+// $(document).ready(function() {
+//     $("#lightSlider").lightSlider({
+//         item: 3,
+//         autoWidth: false,
+//         slideMove: 1, // slidemove will be 1 if loop is true
+//         slideMargin: 10,
+ 
+//         addClass: '',
+//         mode: "slide",
+//         useCSS: true,
+//         cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+//         easing: 'linear', //'for jquery animation',////
+ 
+//         speed: 400, //ms'
+//         auto: false,
+//         loop: false,
+//         slideEndAnimation: true,
+//         pause: 2000,
+ 
+//         keyPress: false,
+//         controls: true,
+//         prevHtml: '',
+//         nextHtml: '',
+ 
+//         rtl:false,
+//         adaptiveHeight:false,
+ 
+//         vertical:false,
+//         verticalHeight:500,
+//         vThumbWidth:100,
+ 
+//         thumbItem:10,
+//         pager: true,
+//         gallery: false,
+//         galleryMargin: 5,
+//         thumbMargin: 5,
+//         currentPagerPosition: 'middle',
+ 
+//         enableTouch:true,
+//         enableDrag:true,
+//         freeMove:true,
+//         swipeThreshold: 40,
+ 
+//         responsive : [],
+ 
+//         onBeforeStart: function (el) {},
+//         onSliderLoad: function (el) {},
+//         onBeforeSlide: function (el) {},
+//         onAfterSlide: function (el) {},
+//         onBeforeNextSlide: function (el) {},
+//         onBeforePrevSlide: function (el) {}
+//     });
+// });
+// $(document).ready(function() {
+//     var slider = $("#lightSlider").lightSlider();
+//     slider.goToSlide(3);
+//     slider.goToPrevSlide();
+//     slider.goToNextSlide();
+//     slider.getCurrentSlideCount();
+//     slider.refresh();
+//     slider.play(); 
+//     slider.pause();    
+//   });
