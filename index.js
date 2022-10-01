90f9c62365b1aaddfed2c924090a53937288df28
Moumita Das <dasmoumita.me@gmail.com>
2022-10-01 23:04:42 +0530

    day5

--- index.js
+++ index.js
@@ -143,3 +143,41 @@ document.querySelector("#login_submit").addEventListener("click",login_fun)
 // let signup_user_name=document.getElementById("signup_user_name")
 // signup_user_name.innerText=signupUser
 // localStorage.setItem("current_signup",JSON.stringify(user.name1))
+
+
+
+// slider
+
+
+let imglink=["https://files.myglamm.com/site-images/original/1920x527sk.jpg","https://files.myglamm.com/site-images/original/TGLF-sept-main-KV-1920x527_2.jpg"]
+let imgState=0
+let left_btn=document.getElementById("left_btn")
+let right_btn=document.getElementById("right_btn")
+let slimg=document.getElementById("slimg")
+
+right_btn.addEventListener("click",()=>{
+    if( imgState==1){
+        return
+    }
+    
+    imgState++
+    slimg.src=imglink[imgState]
+    console.log(imgState)
+})
+
+
+left_btn.addEventListener("click",()=>{
+    if( imgState===0){
+        return
+        
+    }
+    
+    imgState--
+    slimg.src=imglink[imgState]
+    console.log(imgState)
+})
+
+
+
+
+
